f8e6b91c8b6992ae4e61af3ee1b79962d526ccb8
ersinelci <ersinelci@gmail.com>
2023-03-04 21:05:02 +0300

    Bitirme Ödevi First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,39 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { VisitorComponent } from './admin/visitor/visitor.component';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '', component: MainLayoutComponent, children: [
+      {path: 'anasayfa', component: HomeComponent},
+      {path: 'hakkimizda', component: AboutComponent},
+      {path: 'galeri', component: GalleryComponent},
+      {path: 'ziyaretci-sayfasi', component: VisitorComponent}
+    ]
+  },
+
+{
+  path: 'admin', component: AdminLayoutComponent, children:  [
+    {path: 'ziyaretci-sayfasi', component: VisitorComponent}
+  ]
+}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { VisitorComponent } from './admin/visitor/visitor.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VisitorComponent,
+    HomeComponent,
+    GalleryComponent,
+    AboutComponent,
+    AdminLayoutComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainLayoutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
